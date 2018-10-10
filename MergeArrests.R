abc87c8bf1aa9469737afa605d0bdd98561a072d
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 17:32:04 -0400

    Saving MergeArrests.R

--- MergeArrests.R
+++ MergeArrests.R
@@ -3,5 +3,5 @@ arrests <- USArrests
 arrests
 df
 #plot1 <- hist(df$Population)
-merge_df <- merge(arrests,df,by.arrests=row.names(arrests),all="FALSE")
+merge_df <- merge(arrests,merge_df)
 merge_df
