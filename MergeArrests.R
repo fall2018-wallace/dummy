8bd74335f6a753b758b8551eca301f4015a959a3
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 00:23:43 -0400

    Saving MergeArrests.R

--- MergeArrests.R
+++ MergeArrests.R
@@ -8,11 +8,18 @@ arrests
 #3)	Create a merged dataframe -- with the attributes from both dataset
 merge_df <- merge(arrests,df,by="Statename")
 merge_df
+
 #4)	Create a histogram using ggplot2() for the population and a different histogram for 
 #the murder rate
 myplotpop <- ggplot(merge_df, aes(x=Population)) + geom_histogram(binwidth = 500000) + ggtitle("Histogram of Population")
-myplotpop
 myplotmur <- ggplot(merge_df, aes(x=Murder)) + geom_histogram(binwidth = 5)+  ggtitle("Histogram of Murder")
 myplotass <- ggplot(merge_df, aes(x=Assault)) + geom_histogram(binwidth = 50) +  ggtitle("Histogram of Assault")
 myplotrape <- ggplot(merge_df, aes(x=Rape)) + geom_histogram(binwidth = 5) +  ggtitle("Histogram of Rape")
+
+#5)	Create a boxplot for the population, and a different boxplot for the murder rate.
+mybplotpop <- ggplot(merge_df,aes(x=factor(0),y=Population)) + geom_boxplot()+ ggtitle("Boxplot of Population")
+mybplotmur <- ggplot(merge_df,aes(x=factor(0),y=Murder)) + geom_boxplot()+ ggtitle("Boxplot of Murder")
+mybplotass <- ggplot(merge_df,aes(x=factor(0),y=Assault)) + geom_boxplot()+ ggtitle("Boxplot of Assault")
+mybplotrape <- ggplot(merge_df,aes(x=factor(0),y=Rape)) + geom_boxplot()+ ggtitle("Boxplot of Rape")
+
 
