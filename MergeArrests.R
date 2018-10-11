7a8e269af8f5c6ac7f3460a15051fc089a277926
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 00:19:08 -0400

    Saving MergeArrests.R

--- MergeArrests.R
+++ MergeArrests.R
@@ -5,8 +5,11 @@ arrests
 df
 arrests$Statename <- rownames(arrests)
 arrests
+#3)	Create a merged dataframe -- with the attributes from both dataset
 merge_df <- merge(arrests,df,by="Statename")
 merge_df
+#4)	Create a histogram using ggplot2() for the population and a different histogram for 
+#the murder rate
 myplotpop <- ggplot(merge_df, aes(x=Population)) + geom_histogram(binwidth = 500000) + ggtitle("Histogram of Population")
 myplotpop
 myplotmur <- ggplot(merge_df, aes(x=Murder)) + geom_histogram(binwidth = 5)+  ggtitle("Histogram of Murder")
