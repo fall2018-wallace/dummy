b59c2c2c7fb5d2d9c1a21000022c2d505fa371ef
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-11 00:14:37 -0400

    Saving MergeArrests.R

--- MergeArrests.R
+++ MergeArrests.R
@@ -8,6 +8,7 @@ arrests
 merge_df <- merge(arrests,df,by="Statename")
 merge_df
 myplotpop <- ggplot(merge_df, aes(x=population)) + geom_histogram(binwidth = 500000) + ggtitle("Histogram of Population")
+myplotpop
 myplotmur <- ggplot(merge_df, aes(x=Murder)) + geom_histogram(binwidth = 5)+  ggtitle("Histogram of Murder")
 myplotass <- ggplot(merge_df, aes(x=Assault)) + geom_histogram(binwidth = 50) +  ggtitle("Histogram of Assault")
 myplotrape <- ggplot(merge_df, aes(x=Rape)) + geom_histogram(binwidth = 5) +  ggtitle("Histogram of Rape")
